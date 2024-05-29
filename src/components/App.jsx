1eb889bba33018630e0f5261ef6bc5cc6a5fad3d
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2024-05-29 23:22:25 +0400

    add delete function with error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import Stats from "./Stats";
 import PackingList from "./PackingList";
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
 
 function App() {
   const [items, setItems] = useState([]);
@@ -16,11 +10,9 @@ function App() {
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
-
   const handleDelete = (id) => {
-    const newAfterDeleteItem = items.filter((item) => item.id !== id);
-    setItems(newAfterDeleteItem);
-    console.log(items);
+    console.log(id);
+    setItems((items) => items.filter((item) => item.id !== id));
   };
 
   return (
